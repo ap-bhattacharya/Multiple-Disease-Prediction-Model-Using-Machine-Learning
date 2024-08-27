8842c62cacd20c672b6afc0d34af0e4aa532c397
Arunangshu Prasad Bhattacharya <109788065+ap-bhattacharya@users.noreply.github.com>
2024-08-27 13:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/bhatt/OneDrive/Desktop/Multiple Desease Prediction/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/bhatt/OneDrive/Desktop/Multiple Desease Prediction/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/bhatt/OneDrive/Desktop/Multiple Desease Prediction/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
 
 
 
